a2e6b20ceb57b002e3a394c971e8931f0bde3b4f
Noelle Rustad <nprustad@gmail.com>
2015-03-04 10:30:37 -0800

    Complete Authorizaion Assignment

--- app/policies/post_policy.rb
+++ app/policies/post_policy.rb
@@ -1,5 +1,23 @@
 class PostPolicy < ApplicationPolicy
-  def index?
-    true
+  class Scope
+    attr_reader :user, :scope
+
+    def initialize(user, scope)
+      @user = user
+      @scope = scope
+    end
+
+    def resolve
+      if user.present? && (user.role == 'admin' || user.role == 'moderator')
+        scope.all 
+      elsif user.present?
+        scope.where(:user_id => @user.id)      
+      else 
+        []
+      end
+    end
   end
 end
+
+
+  
