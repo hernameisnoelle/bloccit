b3567966ed4562e5f9f462516b9f04f77ec03bff
Noelle Rustad <nprustad@gmail.com>
2015-04-01 23:51:38 -0700

    Uploading Images Assignment

--- Gemfile
+++ Gemfile
@@ -33,6 +33,10 @@ gem 'devise'
 gem 'pundit'
 gem 'figaro', '1.0'
 gem 'redcarpet'
+gem 'carrierwave'
+gem 'mini_magick'
+gem 'fog'
+
 
 # Turbolinks makes following links in your web application faster. Read more: https://github.com/rails/turbolinks
 gem 'turbolinks'
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,13 +4,14 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, :confirmable
   has_many :posts
+    mount_uploader :avatar, AvatarUploader
 
-    def admin?
-        role == 'admin'
-    end
+  def admin?
+    role == 'admin'
+  end
  
-    def moderator?
-        role == 'moderator'
-    end
+  def moderator?
+    role == 'moderator'
+  end
 end
 
